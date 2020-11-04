c6e85c5e810e58f77d0f0b421c258d53241735e2
Vivian Yoyojo Moreno <vyorojo@gmail.com>
2020-11-03 22:46:44 -0300

    VA!!

--- R/biomassify.R
+++ R/biomassify.R
@@ -1,8 +1,8 @@
 biomasa_brac <- function(long) {
   b <- 0.6 * long
   c <- 0.4 * long
-  vol <- 0.52 * long * b *c
-  vol
+  vol <- (0.52 * long * b *c)/1000000
+  peso_seco <- vol/10 + vol/100
 }
 
 biomasa_fili <- function(long) {
@@ -11,7 +11,54 @@ biomasa_fili <- function(long) {
   vol
 }
 
+biomasa_kera <- function(long) {
+  b <- 0.7 * long
+  c <- 0.33 * long
+  vol <- long * b * c
+  vol
+}
 
+biomasa_polyar <- function(long) {
+  b <- 0.7 * long
+  c <- 0.4 * long
+  vol <- long * b * c
+  vol
+}
+
+biomasa_pompho <- function(long) {
+  b <- 0.7 * long
+  c <- 0.5 * long
+  vol <- 0.4 * long * b * c
+  vol
+}
+
+biomasa_testu <- function(long) {
+  b <- long
+  c <- 0.2 * long
+  vol <- 0.4 * long * b * c
+  vol
+}
+
+biomasa_notho <- function(long) {
+  b <- 0.4 * long
+  c <- 0.2 * long
+  vol <- 0.13 * (3 * long * b * c) + 4 * c^3
+  vol
+}
+
+biomasa_asplac <- function(long) {
+  c <- 0.7 * long
+  b <- c
+  vol <- 0.52 * long * b^2
+  vol
+}
+
+biomasa_nauplii <- function(long) {
+  c <- 0.7 * long
+  b <- c
+  vol <- 0.52 * long * b^2
+  vol
+}
 #' Title
 #'
 #' @param especie cadena
@@ -28,9 +75,38 @@ biomassify <- function(especie, long) {
     volumen <- biomasa_brac(long)
   }
 
-  if (stringr::str_detect(especie,"Filinia")) {
+  else if (stringr::str_detect(especie,"Filinia")) {
     volumen <- biomasa_fili(long)
+  }
+
+  else if (stringr::str_detect(especie,"Keratella")) {
+    volumen <- biomasa_kera(long)
+  }
+
+  else if (stringr::str_detect(especie,"Polyarthra")) {
+    volumen <- biomasa_polyar(long)
+}
+
+  else if (stringr::str_detect(especie,"Pompholyx")) {
+    volumen <- biomasa_pompho(long)
+}
+
+  else if (stringr::str_detect(especie,"Testudinella")) {
+    volumen <- biomasa_testu(long)
+  }
+
+  else if (stringr::str_detect(especie,"Notholca")) {
+    volumen <- biomasa_notho(long)
+  }
+
+  else if (stringr::str_detect(especie,"Asplachna")) {
+    volumen <- biomasa_asplac(long)
+  }
+
+  else {
+    volumen <- 0
   }
   volumen
 
 }
+
