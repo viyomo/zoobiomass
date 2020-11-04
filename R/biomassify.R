851649e76eff003dfbceac2f85a0a1b7ee8736cc
Gabriel O. Mendivil <g.mendivil@cidepint.ing.unlp.edu.ar>
2020-11-03 23:24:16 -0300

    pone k, para calcular peso seco

--- R/biomassify.R
+++ R/biomassify.R
@@ -1,112 +1,121 @@
-biomasa_brac <- function(long) {
+biomasa_brac <- function(long, k) {
   b <- 0.6 * long
   c <- 0.4 * long
   vol <- (0.52 * long * b *c)/1000000
-  peso_seco <- vol/10 + vol/100
+  ps <- vol * k + vol/100
+  ps
 }
 
-biomasa_fili <- function(long) {
+biomasa_fili <- function(long, k) {
   b <- 0.5 * long
-  vol <- 0.52 * long * b^2
-  vol
+  vol <- (0.52 * long * b^2) /1000000
+  ps <- vol * k
+  ps
 }
 
-biomasa_kera <- function(long) {
+biomasa_kera <- function(long, k) {
   b <- 0.7 * long
   c <- 0.33 * long
-  vol <- long * b * c
-  vol
+  vol <- (long * b * c) / 1000000
+  ps <- vol * k
+  ps
 }
 
-biomasa_polyar <- function(long) {
+biomasa_polyar <- function(long, k) {
   b <- 0.7 * long
   c <- 0.4 * long
-  vol <- long * b * c
-  vol
+  vol <- (long * b * c) / 1000000
+  ps <- vol *k
+  ps
 }
 
-biomasa_pompho <- function(long) {
+biomasa_pompho <- function(long, k) {
   b <- 0.7 * long
   c <- 0.5 * long
-  vol <- 0.4 * long * b * c
-  vol
+  vol <- (0.4 * long * b * c) /1000000
+  ps <- vol * k
+  ps
 }
 
-biomasa_testu <- function(long) {
+biomasa_testu <- function(long, k) {
   b <- long
   c <- 0.2 * long
-  vol <- 0.4 * long * b * c
-  vol
+  vol <- (0.4 * long * b * c) / 1000000
+  ps <-   vol * k
+  ps
 }
 
-biomasa_notho <- function(long) {
+biomasa_notho <- function(long, k) {
   b <- 0.4 * long
   c <- 0.2 * long
-  vol <- 0.13 * (3 * long * b * c) + 4 * c^3
-  vol
+  vol <- (0.13 * (3 * long * b * c) + 4 * c^3) / 1000000
+  ps <- vol *k
+  ps
 }
 
-biomasa_asplac <- function(long) {
-  c <- 0.7 * long
-  b <- c
-  vol <- 0.52 * long * b^2
-  vol
+biomasa_asplac <- function(long, k) {
+  b <- 0.7 * long
+  vol <- (0.52 * long * b^2) /1000000
+  ps <-  vol *k
+  ps
 }
 
-biomasa_nauplii <- function(long) {
+biomasa_nauplii <- function(long, k) {
   c <- 0.7 * long
   b <- c
-  vol <- 0.52 * long * b^2
-  vol
+  vol <- (0.52 * long * b^2)/1000000
+  ps <-   vol*k
+  ps
 }
-#' Title
+#' Calcula el peso seco
 #'
 #' @param especie cadena
 #' @param long     longitud
+#' @param k     porcentage sin agua
 #'
 #' @return    volumen
 #' @export
 #'
 # @examples
 
-biomassify <- function(especie, long) {
+biomassify <- function(especie, long, k = 0.1) {
 
   if (stringr::str_detect(especie,"Brachionus")) {
-    volumen <- biomasa_brac(long)
+    volumen <- biomasa_brac(long, k)
   }
 
   else if (stringr::str_detect(especie,"Filinia")) {
-    volumen <- biomasa_fili(long)
+    volumen <- biomasa_fili(long, k)
   }
 
   else if (stringr::str_detect(especie,"Keratella")) {
-    volumen <- biomasa_kera(long)
+    volumen <- biomasa_kera(long, k)
   }
 
   else if (stringr::str_detect(especie,"Polyarthra")) {
-    volumen <- biomasa_polyar(long)
+    pseco <- biomasa_polyar(long, k)
 }
 
   else if (stringr::str_detect(especie,"Pompholyx")) {
-    volumen <- biomasa_pompho(long)
+    pseco <- biomasa_pompho(long, k)
 }
 
   else if (stringr::str_detect(especie,"Testudinella")) {
-    volumen <- biomasa_testu(long)
+    pseco <- biomasa_testu(long, k)
   }
 
   else if (stringr::str_detect(especie,"Notholca")) {
-    volumen <- biomasa_notho(long)
+    pseco <- biomasa_notho(long, k)
   }
 
   else if (stringr::str_detect(especie,"Asplachna")) {
-    volumen <- biomasa_asplac(long)
+    pseco <- biomasa_asplac(long, k)
   }
 
   else {
-    volumen <- 0
+    pseco <- 0
   }
-  volumen
+  pseco
 
 }
 
